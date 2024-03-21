17eab051d4c27f270efd643fc54a961728142140
Vincent C <vcl3636@gmail.com>
2024-03-21 13:18:06 +0700

    fix: project structure

---
+++ views-customer/src/App.js
@@ -0,0 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+import React from 'react';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
+}
+
+export default App;
