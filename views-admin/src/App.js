591e15812ab519a592bd14c98621c2a9c1f7a523
feliciakiani <feliciakiani103@gmail.com>
2024-04-03 10:34:31 +0700

    init insert menu admin

--- views-admin/src/App.js
+++ views-admin/src/App.js
@@ -1,28 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Routes, Route } from "react-router-dom";
+import React from 'react';
+import InsertMenu from './pages/InsertMenu/InsertMenu.jsx';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1 className="text-xl font-bold underline">
-          Hello world!
-        </h1>
-      </header>
-    </div>
+    <>
+      {/* <div className="page-container"> */}
+        <Routes>
+          <Route path="/insert-menu" element={<InsertMenu />} />
+        </Routes>
+      {/* </div> */}
+    </>
   );
 }
 
 export default App;
+
