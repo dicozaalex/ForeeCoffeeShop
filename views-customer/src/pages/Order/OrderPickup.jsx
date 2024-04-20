9f9cc5f96c4e2f6f3390cb4d24eab1a9f7b69911
Vincent C <vcl3636@gmail.com>
2024-04-20 17:01:00 +0700

    fix: global css

--- views-customer/src/pages/Order/OrderPickup.jsx
+++ views-customer/src/pages/Order/OrderPickup.jsx
@@ -8,24 +8,24 @@ function OrderPickup() {
             <div className={styles.fullScreen}>
                 <div className={styles.information}>
                     <div className={styles.infoItem}>
-                        <img className={styles.circledImage} src={`${process.env.PUBLIC_URL}/assets/Order/shopping-bag.png`} alt="shopping bag" />
+                        <img className={`${styles.img} ${styles.circledImage}`} src={`${process.env.PUBLIC_URL}/assets/Order/shopping-bag.png`} alt="shopping bag" />
                         <div className={styles.infoText}>
                             <span>Pickup</span>
                             <span>Pickup at the store of your choice</span>
                         </div>
                     </div>
                     <br />
-                    <h1>Take Your Order At</h1>
+                    <h1 className={styles.h1}>Take Your Order At</h1>
                     <hr style={{ border: '0', borderTop: '2px solid white', marginTop: '5px' }} />
                     <div className={styles.infoItem} style={{ marginTop: '20px' }}>
-                        <img src={`${process.env.PUBLIC_URL}/assets/ViewOrder/store.png`} alt="store" />
+                        <img className={styles.img} src={`${process.env.PUBLIC_URL}/assets/ViewOrder/store.png`} alt="store" />
                         <div className={styles.infoText}>
                             <span>Branch</span>
                             <span>Dipatiukur, Bandung</span>
                         </div>
                     </div>
                     <div className={styles.infoItem} style={{ marginTop: '20px' }}>
-                        <img src={`${process.env.PUBLIC_URL}/assets/ViewOrder/user.png`} alt="user" />
+                        <img className={styles.img} src={`${process.env.PUBLIC_URL}/assets/ViewOrder/user.png`} alt="user" />
                         <div className={styles.infoText}>
                             <span>Name</span>
                             <span>Felis</span>
@@ -36,7 +36,7 @@ function OrderPickup() {
                 </div>
                 <div className={styles.order}>
                     <div className={styles.orderHeader}>
-                        <h1>Order Details</h1>
+                        <h1 className={styles.h1}>Order Details</h1>
                         <Link to="/coffee" className={styles['addButton']}>
                             <span style={{ color: '#1C5739', margin: 'auto' }}>Add</span>
                         </Link>
@@ -45,37 +45,37 @@ function OrderPickup() {
                     <div className={styles.orderList}>
                         <div className={styles.orderName}>
                             <span>Matcha Green Tea</span>
-                            <img src={`${process.env.PUBLIC_URL}/assets/ViewOrder/matcha-green-tea.png`} alt="menu" />
+                            <img className={styles.img} src={`${process.env.PUBLIC_URL}/assets/ViewOrder/matcha-green-tea.png`} alt="menu" />
                         </div>
                         <div className={styles.orderDetails}>
                             <div>
                                 <span>Rp 10.000</span>
                             </div>
                             <div className={styles.orderItems}>
-                                <img src={`${process.env.PUBLIC_URL}/assets/Order/minus.png`} alt="minus" />
+                                <img className={styles.img} src={`${process.env.PUBLIC_URL}/assets/Order/minus.png`} alt="minus" />
                                 <span>2</span>
-                                <img src={`${process.env.PUBLIC_URL}/assets/Order/plus.png`} alt="plus" />
+                                <img className={styles.img} src={`${process.env.PUBLIC_URL}/assets/Order/plus.png`} alt="plus" />
                             </div>
                         </div>
                     </div>
                     <div className={styles.orderList}>
                         <div className={styles.orderName}>
                             <span>Strawberry Donut</span>
-                            <img src={`${process.env.PUBLIC_URL}/assets/ViewOrder/strawberry-donut.png`} alt="menu" />
+                            <img className={styles.img} src={`${process.env.PUBLIC_URL}/assets/ViewOrder/strawberry-donut.png`} alt="menu" />
                         </div>
                         <div className={styles.orderDetails}>
                             <div>
                                 <span>Rp 10.000</span>
                             </div>
                             <div className={styles.orderItems}>
-                                <img src={`${process.env.PUBLIC_URL}/assets/Order/minus.png`} alt="minus" />
+                                <img className={styles.img} src={`${process.env.PUBLIC_URL}/assets/Order/minus.png`} alt="minus" />
                                 <span>1</span>
-                                <img src={`${process.env.PUBLIC_URL}/assets/Order/plus.png`} alt="plus" />
+                                <img className={styles.img} src={`${process.env.PUBLIC_URL}/assets/Order/plus.png`} alt="plus" />
                             </div>
                         </div>
                     </div>
                     <br />
-                    <h1>Total Payment</h1>
+                    <h1 className={styles.h1}>Total Payment</h1>
                     <hr style={{ border: '0', borderTop: '2px solid white', marginTop: '5px' }} />
                     <div className={styles.orderTotal}>
                         <div className={styles.orderTotalPrice}>
